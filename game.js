b5c8826625994afa97f5bcbedac71b4148cd14b8
Evgeniy Kalimullin <counsil@mail.ru>
2022-01-16 17:56:01 +0700

    added coordinates for ball and platform

--- game.js
+++ game.js
@@ -5,6 +5,16 @@ let game = {
     ball: null,
     platform: null,
   },
+  ball: {
+    x: 320,
+    y: 280,
+    width: 20,
+    height: 20,
+  },
+  platform: {
+    x: 280,
+    y: 300,
+  },
   init: function () {
     this.ctx = document.getElementById('mycanvas').getContext('2d')
   },
@@ -19,8 +29,18 @@ let game = {
   run: function () {
     window.requestAnimationFrame(() => {
       this.ctx.drawImage(this.sprites.background, 0, 0)
-      this.ctx.drawImage(this.sprites.ball, 0, 0)
-      this.ctx.drawImage(this.sprites.platform, 0, 0)
+      this.ctx.drawImage(
+        this.sprites.ball,
+        0,
+        0,
+        this.ball.width,
+        this.ball.height,
+        this.ball.x,
+        this.ball.y,
+        this.ball.width,
+        this.ball.height
+      )
+      this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y)
     })
   },
   start: async function () {
