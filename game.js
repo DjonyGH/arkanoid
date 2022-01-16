f79c59f647a8c8c35ca7ed2a487e9d4898d703cd
Evgeniy Kalimullin <counsil@mail.ru>
2022-01-16 17:38:42 +0700

    created ball and platform, refactoring code

--- game.js
+++ game.js
@@ -1,14 +1,35 @@
 let game = {
-    start: function() {
-        this.ctx = document.getElementById("mycanvas").getContext("2d");
-        let background = new Image();
-        background.src = "img/background.png";
-        window.requestAnimationFrame(() => {
-            this.ctx.drawImage(background, 0, 0);
-        });
+  ctx: null,
+  sprites: {
+    background: null,
+    ball: null,
+    platform: null,
+  },
+  init: function () {
+    this.ctx = document.getElementById('mycanvas').getContext('2d')
+  },
+  preload: async function () {
+    for (let key in this.sprites) {
+      this.sprites[key] = new Image()
+      this.sprites[key].src = `img/${key}.png`
+      await this.sprites[key].decode()
+      console.log(`${this.sprites[key]} width: ${this.sprites[key].width}`)
     }
-};
+  },
+  run: function () {
+    window.requestAnimationFrame(() => {
+      this.ctx.drawImage(this.sprites.background, 0, 0)
+      this.ctx.drawImage(this.sprites.ball, 0, 0)
+      this.ctx.drawImage(this.sprites.platform, 0, 0)
+    })
+  },
+  start: async function () {
+    this.init()
+    await this.preload()
+    this.run()
+  },
+}
 
-window.addEventListener("load", () => {
-    game.start();
-});
+window.addEventListener('load', () => {
+  game.start()
+})
