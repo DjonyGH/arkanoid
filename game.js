08102f21a7dc21e863b2032ca85250496e18bdfc
Evgeniy Kalimullin <counsil@mail.ru>
2022-01-17 15:22:37 +0700

    added move platform

--- game.js
+++ game.js
@@ -16,11 +16,25 @@ let game = {
     height: 20,
   },
   platform: {
+    velocity: 6,
+    dx: 0,
     x: 280,
     y: 300,
+    move() {
+      this.dx && (this.x += this.dx)
+    },
   },
   init: function () {
     this.ctx = document.getElementById('mycanvas').getContext('2d')
+  },
+  setEvents: function () {
+    window.addEventListener('keydown', (e) => {
+      e.code === 'ArrowLeft' && (this.platform.dx = -this.platform.velocity)
+      e.code === 'ArrowRight' && (this.platform.dx = this.platform.velocity)
+    })
+    window.addEventListener('keyup', (e) => {
+      this.platform.dx = 0
+    })
   },
   preload: async function () {
     for (let key in this.sprites) {
@@ -40,28 +54,37 @@ let game = {
       }
     }
   },
+  render: function () {
+    this.ctx.drawImage(this.sprites.background, 0, 0)
+    this.ctx.drawImage(
+      this.sprites.ball,
+      0,
+      0,
+      this.ball.width,
+      this.ball.height,
+      this.ball.x,
+      this.ball.y,
+      this.ball.width,
+      this.ball.height
+    )
+    this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y)
+    for (let block of this.blocks) {
+      this.ctx.drawImage(this.sprites.block, block.x, block.y)
+    }
+  },
+  update: function () {
+    this.platform.move()
+  },
   run: function () {
     window.requestAnimationFrame(() => {
-      this.ctx.drawImage(this.sprites.background, 0, 0)
-      this.ctx.drawImage(
-        this.sprites.ball,
-        0,
-        0,
-        this.ball.width,
-        this.ball.height,
-        this.ball.x,
-        this.ball.y,
-        this.ball.width,
-        this.ball.height
-      )
-      this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y)
-      for (let block of this.blocks) {
-        this.ctx.drawImage(this.sprites.block, block.x, block.y)
-      }
+      this.update()
+      this.render()
+      this.run()
     })
   },
   start: async function () {
     this.init()
+    this.setEvents()
     await this.preload()
     this.create()
     this.run()
